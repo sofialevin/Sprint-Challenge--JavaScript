5f43a47f95ebabbe368898b01bb5049e36388d89
sofialevin <slevin@M-4876.local>
2019-06-01 10:04:19 -0700

    Completed ADVANCED Array Methods challenge in objects-arrays.js.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -133,6 +133,11 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+
+zooAnimals.forEach(function(zooAnimals) {
+  animalNames.push(`Name: ${zooAnimals.animal_name}, Scientific: ${zooAnimals.scientific_name}`)
+})
+
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -141,7 +146,12 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+let lowerCase = [];
+
+lowerCase = zooAnimals.map(function(zooAnimals) {
+  return zooAnimals.animal_name.toLowerCase();
+})
+
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -149,7 +159,12 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
+let lowerPopulation = [];
+
+lowerPopulation = zooAnimals.filter(function(zooAnimals) {
+  return zooAnimals.population < 5;
+})
+
 console.log(lowerPopulation);
 
 /* Request 4: .reduce() 
@@ -157,9 +172,13 @@ console.log(lowerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+let populationTotal = 0;
 
+populationTotal = zooAnimals.reduce(function(total, zooAnimals) {
+  return total = total + zooAnimals.population
+}, 0)
+
+console.log(populationTotal);
 
 /* 
 
